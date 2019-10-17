672d9884c817dc3ab16205471b9705f3915ffd7d
Yu Shi Qiang <shiqiang.yu@siemens.com>
2019-10-17 10:18:07 +0800

    first commit

---
+++ src/main/java/com/thanos/sasl/SaslTestApplication.java
@@ -0,0 +1,13 @@
+package com.thanos.sasl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaslTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaslTestApplication.class, args);
+	}
+
+}
